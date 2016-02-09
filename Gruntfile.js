ca197a91ecfd5a654425a2d5f24e1d901ef58b16
Evan Van Dam <evandam92@gmail.com>
2016-02-09 18:44:19 -0500

    fixed path to server.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -93,8 +93,8 @@ module.exports = function (grunt) {
   });
 
   grunt.registerTask('build', ['clean', 'copy', 'compass', 'requirejs']);
-  grunt.registerTask('heroku:development', ['build']);
-  grunt.registerTask('heroku:production', ['build']);
+  grunt.registerTask('heroku:development', ['build', 'serve']);
+  grunt.registerTask('heroku:production', ['heroku:development']);
   grunt.registerTask('default', ['build', 'concurrent']);
 
 };
